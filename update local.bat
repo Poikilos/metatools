ad4dffe5943e3366ce693103d307473f4050172c
Jacob Gustafson <jgustafson@fbm.local>
2017-05-19 17:07:45 -0400

    added destination detection

--- update local.bat
+++ update local.bat
@@ -1,4 +1,6 @@
-copy /y *.lua C:\games\Minetest\games\ENLIVEN\mods\metatools\
-copy /y *.md C:\games\Minetest\games\ENLIVEN\mods\metatools\
-copy /y textures\*.png C:\games\Minetest\games\ENLIVEN\mods\metatools\textures\
+SET MT_PROGRAM_DIR=C:\games\Minetest
+IF EXIST "C:\Games\ENLIVEN" SET MT_PROGRAM_DIR=C:\Games\ENLIVEN
+copy /y *.lua "%MT_PROGRAM_DIR%\games\ENLIVEN\mods\metatools\"
+copy /y *.md "%MT_PROGRAM_DIR%\games\ENLIVEN\mods\metatools\"
+copy /y textures\*.png "%MT_PROGRAM_DIR%\games\ENLIVEN\mods\metatools\textures\"
 if NOT ["%errorlevel%"]==["0"] pause
