b70ba39c17ac91cfd1e7bcc1d065cc03e7996f99
Jacob Gustafson <jgustafson@fbm.local>
2017-05-19 17:05:11 -0400

    created batch for testing purposes only

---
+++ update local.bat
@@ -0,0 +1,4 @@
+copy /y *.lua C:\games\Minetest\games\ENLIVEN\mods\metatools\
+copy /y *.md C:\games\Minetest\games\ENLIVEN\mods\metatools\
+copy /y textures\*.png C:\games\Minetest\games\ENLIVEN\mods\metatools\textures\
+if NOT ["%errorlevel%"]==["0"] pause
